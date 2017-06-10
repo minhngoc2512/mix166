a307551ed4fd56a3b0a34e6c06480ca0ffbee7df
minhngoc2512 <minhngoc2512@yahoo.com>
2017-06-10 23:53:32 +0700

    minh

--- app/commons/DBFactory.php
+++ app/commons/DBFactory.php
@@ -7,6 +7,8 @@
 		public static function getInstance(){
 			if(!isset(self::$conn)){
 				try{
+
+					
 				    self::$conn = new mysqli('localhost','root','admin','mix166');
 				    self::$conn->query('names set utf8');
 				}catch(PDOException $e){
